3450c5ef68b8f7a849748052d8f53c98041789ef
Grzegorz Dyl <reksetoajxer@gmail.com>
2016-12-04 22:40:51 +0100

    Created base for documentation.

--- heix.js
+++ heix.js
@@ -1,7 +1,17 @@
 var heix = {
     routes: [],
-    addRoute: function (arguments) {
-        this.routes.push({mode: arguments.mode, selector: arguments.selector});
+    addRoute: function (mode, selector) {
+
+        /**
+         * @func heix#addRoute
+         * @desc registring EventListener click for all of elements from selector to change url by History API
+         * @param {String} mode data
+         * @param {Selector} selector='body' - CSS Selector
+         * @example
+         *  heix.addRoute({mode: 'data', selector: '.navigation-list-element-link'});
+         *  heix.loadRouters();
+         */
+        this.routes.push({mode: this.mode, selector: this.selector});
     },
     loadRouters: function () {
         [].forEach.call(this.routes, function (route) {
