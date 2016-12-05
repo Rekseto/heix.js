2887751ae7843a5b8e392c89807911634162655d
Rekseto <reksetoajxer@gmail.com>
2016-12-05 21:54:55 +0100

    Added callbacks, added waffquery integration , main function from now is addRoute.

--- heix.js
+++ heix.js
@@ -1,31 +1,28 @@
 var heix = {
     routes: [],
-    addRoute: function (mode, selector) {
-
-        /**
-         * @func heix#addRoute
-         * @desc registring EventListener click for all of elements from selector to change url by History API
-         * @param {String} mode data
-         * @param {Selector} selector='body' - CSS Selector
-         * @example
-         *  heix.addRoute({mode: 'data', selector: '.navigation-list-element-link'});
-         *  heix.loadRouters();
-         */
-        this.routes.push({mode: this.mode, selector: this.selector});
-    },
-    loadRouters: function () {
-        [].forEach.call(this.routes, function (route) {
-            var list = Array.prototype.slice.call(document.querySelectorAll(route.selector));
-            [].forEach.call(list, function (el) {
-                el.addEventListener('click', function (e) {
-                    var data = e.target.getAttribute('data-name'),
-                        url = data;
-                    history.pushState(null, null, url);
-                    e.preventDefault();
-                })
+    /**
+     * @func heix#addRoute
+     * @desc registring EventListener click for all of elements from selector to change url by History API
+     * @param {String} mode data
+     * @param {Selector} selector='body' - CSS Selector
+     * @param {Function} callback
+     *  (function() { heix.addRoute('data', '.navigation-list-element-link', function (route) { console.log(route.mode)});})();
+     */
+    addRoute: function (_mode, _selector, _fnc) {
+        var route = {mode: _mode, selector: _selector, fnc: _fnc};
+        var list = Array.prototype.slice.call(waff.qq(route.selector, null, false, true));
+        [].forEach.call(list, function (element) {
+            element.addEventListener('click', function (e) {
+                var data = e.target.getAttribute('data-name'),
+                    url = data;
+                history.pushState(null, null, url);
+                route.fnc(route);
+                e.preventDefault();
             });
+        });
 
-        })
+
+        this.routes.push(this.route);
     }
 
 }
